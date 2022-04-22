64580b630a2dc09e20e9326c16a081e3f8b29f95
Dimitri-de-bie <Dimitriproject3@gmail.com>
2022-04-22 15:57:18 +0200

    opdracht1

--- index.js
+++ index.js
@@ -21,6 +21,140 @@ app.get("/", (req, res) => {
   });
 });
 
+//user toevoegen
+app.post("/api/user", (req, res) => {
+  let user = req.body;
+  let email = req.body.email;
+  let controle;
+
+  for (let i = 0; i < database.length; i++) {
+    if (database[i].email == email) {
+      controle = false;
+    }
+  }
+  if (controle != false) {
+    id++;
+    user = {
+      id,
+      ...user,
+    };
+    console.log(user);
+    database.push(user);
+    res.status(201).json({
+      status: 201,
+      result: database,
+    });
+  } else {
+    res.status(401).json({
+      status: 401,
+      result: "email bestaat al",
+    });
+  }
+});
+
+//specifieke user ophalen
+app.get("/api/user/:userId", (req, res, next) => {
+  const userId = req.params.userId;
+  console.log(`user met ID ${userId} gezocht`);
+  let user = database.filter((item) => item.id == userId);
+  if (user.length > 0) {
+    console.log(user);
+    res.status(200).json({
+      status: 200,
+      result: user,
+    });
+  } else {
+    res.status(401).json({
+      status: 401,
+      result: `User with ID ${userId} not found`,
+    });
+  }
+});
+
+//specifieke user updaten
+app.post("/api/user/:userId", (req, res, next) => {
+  const userId = req.params.userId;
+  console.log(`user met ID ${userId} gezocht`);
+
+  let controle;
+  let deletedeze;
+
+  for (let i = 0; i < database.length; i++) {
+    if (database[i].id == userId) {
+      controle = true;
+      deletedeze = i;
+      break;
+    }
+  }
+
+  if (controle == true) {
+    database.splice(deletedeze, 1);
+    let user = req.body;
+    id = parseInt(userId);
+    user = {
+      id,
+      ...user,
+    };
+    console.log(user);
+    database.push(user);
+    res.status(201).json({
+      status: 201,
+      result: database,
+    });
+  } else {
+    res.status(401).json({
+      status: 401,
+      result: `User with ID ${userId} not found`,
+    });
+  }
+});
+
+//specifieke user deleten
+app.delete("/api/user/:userId", (req, res, next) => {
+  const userId = req.params.userId;
+  console.log(`user met ID ${userId} gezocht`);
+  let user = database.filter((item) => item.id == userId);
+
+  let controle;
+  let deletedeze;
+
+  for (let i = 0; i < database.length; i++) {
+    if (database[i].id == userId) {
+      controle = true;
+      deletedeze = i;
+      break;
+    }
+  }
+
+  if (controle == true) {
+    database.splice(deletedeze, 1);
+    console.log(user);
+    res.status(200).json({
+      status: 200,
+      result: "user has been deleted",
+    });
+  } else {
+    res.status(401).json({
+      status: 401,
+      result: `User with ID ${userId} not found`,
+    });
+  }
+});
+//alle users ophalen
+app.get("/api/user", (req, res, next) => {
+  res.status(200).json({
+    status: 200,
+    result: database,
+  });
+});
+//personal user ophalen
+app.get("/api/personaluser", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    result: "deze funtionaliteit is nog niet geraliseerd",
+  });
+});
+
 app.post("/api/movie", (req, res) => {
   let movie = req.body;
   id++;
