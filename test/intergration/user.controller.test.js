1e301e382426e4cee085e7889931e39895371924
Dimitri-de-bie <Dimitriproject3@gmail.com>
2022-04-29 09:09:39 +0200

    nales

---
+++ test/intergration/user.controller.test.js
@@ -0,0 +1,42 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../../index");
+
+let database = [];
+
+chai.should();
+chai.use(chaiHttp);
+
+describe("Manage users", () => {
+  describe("UC-201 add users /api/movie", () => {
+    beforeEach((done) => {
+      database = [];
+      done();
+    });
+
+    it("When a required input is missing, a valid error should be returned", (done) => {
+      chai
+        .request(server)
+        .post("/api/user")
+        .send({
+          //Firstname is missing
+          lastName: "achternaam",
+          password: "password",
+          street: "straat",
+          city: "stad",
+          emailAdress: "email@hotmail.com",
+          phoneNumber: "0612345678",
+        })
+        .end((err, res) => {
+          res.should.be.an("object");
+          let { status, result } = res.body;
+          status.should.equals(400);
+          result.should.be
+            .a("string")
+            .that.equals("Firstname must be a string");
+
+          done();
+        });
+    });
+  });
+});
