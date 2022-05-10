c0a9b8ebd60251eed536b7d50a36a650cc18c9ef
Dimitri-de-bie <Dimitriproject3@gmail.com>
2022-05-10 14:43:27 +0200

    sql

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const assert = require("assert");
+const dbconnection = require("../../database");
 let database = [];
 let id = 0;
 
@@ -62,9 +63,32 @@ let controller = {
     }
   },
   getAllUsers: (req, res) => {
-    res.status(200).json({
-      status: 200,
-      result: database,
+    // res.status(200).json({
+    //   status: 200,
+    //   result: database,
+    // });
+
+    dbconnection.getConnection(function (err, connection) {
+      if (err) throw err; // not connected!
+
+      // Use the connection
+      connection.query("SELECT * FROM user", function (error, results, fields) {
+        // When done with the connection, release it.
+        connection.release();
+
+        // Handle error after the release.
+        if (error) throw error;
+
+        // Don't use the connection here, it has been returned to the pool.
+        console.log("#results = ", results.length);
+        res.status(200).json({
+          statusCode: 200,
+          results: results,
+        });
+        // dbconnection.end((err) => {
+        //   console.log("pool was closed.");
+        // });
+      });
     });
   },
   getUserById: (req, res, next) => {
